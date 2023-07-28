43081a275ca0c834010f9abe95549853e9d7c7cc
Russell <russell.ed.123@gmail.com>
2023-07-28 19:58:26 -0300

    Criação da página principal como modelo para as demais (header, main e footer)

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header><hr><h2>Desenvolvimento WEB</h2><hr></header>
+    <main>
+    <h1>Questões:</h1>
+    <ul>
+        <li><a href="./quest1.php">Questão 1</a></li>
+        <li><a href="./quest2.php">Questão 2</a></li>
+        <li><a href="./quest3.php">Questão 3</a></li>
+        <li><a href="./quest4.php">Questão 4</a></li>
+    </ul>
+    </main>
+    <footer>
+        <hr>
+        <p>Russell Edward - &copy; 2023 - all rights reserved</p>
+        <hr>
+    </footer>
+</body>
+</html>
