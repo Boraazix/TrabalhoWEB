ae6245f1a6b424bf32e3ec74f67d074c2538beeb
Russell <russell.ed.123@gmail.com>
2023-08-04 20:45:37 -0300

    add js

---
+++ code.js
@@ -0,0 +1,11 @@
+function defaultValues(){
+    const p1 = window.document.getElementById('p1');
+    const p2 = window.document.getElementById('p2');
+    const h1 = window.document.getElementById('h1');
+    const h2 = window.document.getElementById('h2');
+
+    p1.value=46;
+    p2.value=120;
+    h1.value=146;
+    h2.value=210;
+}
